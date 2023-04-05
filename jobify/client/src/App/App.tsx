c6709e01bf3d954cfdc5c33740160ec5509dcbf5
Teddy Brault <teddy.brault@thalesgroup.com>
2023-04-05 14:02:27 +0200

    setup firsts routes

--- jobify/client/src/App/App.tsx
+++ jobify/client/src/App/App.tsx
@@ -1,7 +1,28 @@
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Landing from '../pages/Landing/Landing';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Jobify</div>,
+    errorElement: <div>error</div>,
+  },
+  {
+    path: 'landing',
+    element: <Landing />,
+  },
+  {
+    path: 'register',
+    element: <div>register</div>,
+  },
+]);
+
 function App() {
-  return <Landing />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- jobify/client/src/pages/Landing/Landing.tsx
+++ jobify/client/src/pages/Landing/Landing.tsx
@@ -15,11 +15,14 @@ function Landing(): JSX.Element {
             job <span>traking</span> app
           </h1>
           <p>
-            This app allow to handle job applications with statistics. It will
-            be my showcase MERN website until I will develop a more complex new
-            one. Please click on the button below in order to register if it is
-            your first visit. Enjoy !!
+            This web application allows to handle job applications with
+            statistics. It will be my MERN website showcase until I will develop
+            a more complex one. Please click on the button below in order to
+            register if it is your first visit. Enjoy !!
           </p>
+          <Link to="/register" className="btn btn-hero">
+            login/register
+          </Link>
         </article>
         <img className="img main-img" src={main} alt="main image" />
       </div>
